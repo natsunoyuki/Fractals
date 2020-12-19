3234e938dfb5bc757b92997c01d1425fb2a3e338
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-12-19 12:57:58 +0900

    Update weierstrauss_fractal.py

--- weierstrauss_fractal.py
+++ weierstrauss_fractal.py
@@ -2,6 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def weierstrauss_fractal(x0 = -1, x1 = 1, Nx = 2000, a = 0.5, n_iter = 20):
+    """
+    Generate the weierstrauss fractal
+    
+    Inputs
+    ------
+    x0: float
+        start point of the x axis
+    x1: float
+        end point of the x axis
+    Nx: int
+        number of points to use along the x axis
+    a: float
+        weierstrauss fractal factor
+    n_iter: int
+        number of iterations to run
+    """
     x = np.linspace(x0, x1, Nx)
     f = np.zeros(len(x))
     b = int((1 + 3 / 2 * np.pi) / a + 1)
