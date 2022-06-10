dd1cb74f56cd73e7eb9c7efc065e04cf1db81555
Юукий Нацумэ <33706936+natsunoyuki@users.noreply.github.com>
2022-06-10 23:21:58 +0900

    Add files via upload

--- src/fractals/__init__.py
+++ src/fractals/__init__.py
@@ -1,9 +1,9 @@
 # __init__.py
 
-from cantor_box import CantorBox
-from fern import BarnsleyFern
-from julia_set import JuliaSet
-from mandelbrot_set import MandelbrotSet
-from sierpinski_carpet import SierpinskiCarpet
-from sierpinski_triangle import SierpinskiTriangle
-from weierstrauss_fractal import WeierstraussFractal
+from .cantor_box import CantorBox
+from .fern import BarnsleyFern
+from .julia_set import JuliaSet
+from .mandelbrot_set import MandelbrotSet
+from .sierpinski_carpet import SierpinskiCarpet
+from .sierpinski_triangle import SierpinskiTriangle
+from .weierstrauss_fractal import WeierstraussFractal
